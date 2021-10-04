d517268b0ec474e2c3c02370aec5c04e33d9fef2
Mary <mariyapiligrim3@gmail.com>
2021-10-04 12:26:05 +0300

    Add new structure

--- src/components/Usercard/index.jsx
+++ src/components/Usercard/index.jsx
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
-import Name from "./../Name";
+import PropTypes from "prop-types"
 import Image from "../Image";
 import Description from "../Description";
 
 class UserCard extends Component {
   render() {
-    const {firstName, lastName, profilePicture } = this.props;
-
+    const { firstName, lastName, profilePicture } = this.props;
     return (
-      <div className = "usercardWrapper">
-        <Image 
+      <div className="usercardWrapper">
+        <Image
           profilePicture={profilePicture}
           firstName={firstName}
           lastName={lastName}
         />
-        <Name firstName={firstName} lastName={lastName} />
+        <h1 className="fullName">
+          {firstName}
+          {lastName}
+        </h1>
         <Description />
       </div>
     );
   }
 }
 
+UserCard.propTypes ={
+  firstName: PropTypes.string,
+  lastName: PropTypes.string
+}
 export default UserCard;
